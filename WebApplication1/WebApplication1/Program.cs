4c5ef4e04503d584b5564ca7a79c3d50d2ce1d7d
gilsa3 <59648041+deadly91@users.noreply.github.com>
2022-04-07 02:29:33 -0400

    added field

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace WebApplication1
 {
     public class Program
     {
+		int x;
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();

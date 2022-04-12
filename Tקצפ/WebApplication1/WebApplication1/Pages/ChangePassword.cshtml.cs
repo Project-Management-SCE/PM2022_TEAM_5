789a86e1891954a64d98a8d79d887c7b1131618a
gilsa3 <59648041+deadly91@users.noreply.github.com>
2022-04-12 11:28:01 -0400

    BSPM2022T5-12

---
+++ Tקצפ/WebApplication1/WebApplication1/Pages/ChangePassword.cshtml.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Pages
+{
+    public class ChangePasswordModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInMannager;
+
+        [BindProperty]
+        public ChangePassword Model { get; set; }
+
+        public ChangePasswordModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInMannager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync(ChangePassword model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return Page();
+                }                
+                var result = await userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+                if (!result.Succeeded)
+                {
+                    foreach (var err in result.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+                }
+
+                await signInMannager.RefreshSignInAsync(user);
+            }
+            
+            return RedirectToPage("Index");
+
+        }
+    }
+}
+
---
+++ WebApplication1/WebApplication1/Pages/register.cshtml.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Pages
+{
+    public class registerModel : PageModel
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        [BindProperty]
+        public Register Model { get; set; }
+
+        public registerModel(UserManager<IdentityUser>UserManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager; ;
+            this.userManager = UserManager;
+            this.signInManager = signInManager;
+        }
+        public void OnGet()
+        {
+        }
+
+        
+        
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(ModelState.IsValid)
+            {
+              
+
+                var user = new IdentityUser()
+                {
+                    UserName = Model.UserName,
+                    Email = Model.Email,                    
+                };
+
+                var  result = await userManager.CreateAsync(user, Model.Password);
+                if (result.Succeeded)
+                {
+                    var role = "Standard";
+                    if (Model.Role)
+                    {
+                        role = "Vip";
+                    }
+                    
+                    await signInManager.SignInAsync(user, false);
+                    await userManager.AddToRoleAsync(user, role);
+                    return RedirectToPage("Index");
+                }
+                foreach(var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+
+            return Page();
+        }
+
+
+
+    }
+}

33b17440ef899aa70b6bd3ebbf90bf399bb926e1
Eliraas1 <eliraas1@ac.sce.ac.il>
2022-05-22 13:18:09 +0300

    dockerfile edited

--- WebApplication1/Automation/UnitTest1.cs
+++ WebApplication1/Automation/UnitTest1.cs
@@ -34,7 +34,7 @@ namespace Automation
             //options.AddAdditionalChromeOption("network.proxy.http_port", "8080");
             //options.AddAdditionalCapability(CapabilityType.WebSocketUrl, "http://127.0.0.1:4444");
             // FirefoxOptions options = new FirefoxOptions();
-            options.AddArgument("--headless");
+            options.AddArgument("--headless");            
             ////options.BinaryLocation = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             //options.AddArguments("disable-infobars"); // disabling infobars
             //options.AddArguments("disable-extensions"); // disabling extensions
@@ -60,10 +60,10 @@ namespace Automation
             //cliProcess.Close();
             //Console.WriteLine(cliOut);
             string driverDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            _driver = new ChromeDriver(driverDirectory, options, TimeSpan.FromSeconds(10));
-            //options.BinaryLocation = driverDirectory;
-            Console.WriteLine("\n\n\tblahhhh\n\n");
-            //_driver = new RemoteWebDriver(options);
+            //_driver = new ChromeDriver(driverDirectory, options, TimeSpan.FromSeconds(10));
+            options.BinaryLocation = driverDirectory;
+            //Console.WriteLine("\n\n\tblahhhh\n\n");
+            _driver = new RemoteWebDriver(options);
             Console.WriteLine("\n\n\tend\n\n");
 
 

0894b9f07beed597d3b3e6092036a0c3edc2a52b
omeratt <73067552+omeratt@users.noreply.github.com>
2022-05-25 19:06:59 +0300

    Update APIKey

--- WebApplication1/WebApplication1/Model/Global.cs
+++ WebApplication1/WebApplication1/Model/Global.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.Model
 {
     public static class Global
     {
-        public static string API_KEY = "bd53cd8106msh092bce5011267a3p1e858fjsne97ace1ebafc";
+        public static string API_KEY = "a76c46f9d6msh7e2e78db8ec4970p105cddjsned16fc451f54";
         public static string CONTACT_US_EMAIL = "team5SCE@gmail.com";
         public static string CONTACT_US_PASSWORD = "OfirGilOmer123!";
     }

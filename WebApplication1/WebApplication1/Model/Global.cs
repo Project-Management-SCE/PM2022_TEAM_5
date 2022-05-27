800105b0b7f57f85c7ffdee196a1e5827df239c7
Eliran Ashtamker <73073303+Eliraas1@users.noreply.github.com>
2022-05-27 23:20:31 +0300

    update api key and jenkins

--- WebApplication1/WebApplication1/Model/Global.cs
+++ WebApplication1/WebApplication1/Model/Global.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.Model
 {
     public static class Global
     {
-        public static string API_KEY = "5ac20b84a7msh6cf881c5f7821e4p1a179ajsnc20ccfa61434";
+        public static string API_KEY = "ef2721cc04msh7603cfac40bcdb1p16ff80jsn53e46fc2ecdb";
         public static string CONTACT_US_EMAIL = "team5SCE@gmail.com";
         public static string CONTACT_US_PASSWORD = "OfirGilOmer123!";
     }

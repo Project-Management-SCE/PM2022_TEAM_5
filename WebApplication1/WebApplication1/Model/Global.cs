8ced168d28ece528b819ae36ee1450daea533a51
Eliran Ashtamker <73073303+Eliraas1@users.noreply.github.com>
2022-05-31 17:19:45 +0300

    New API Key

--- WebApplication1/WebApplication1/Model/Global.cs
+++ WebApplication1/WebApplication1/Model/Global.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.Model
 {
     public static class Global
     {
-        public static string API_KEY = "6bc60b06d1msh9458120f2562d86p1eca1cjsn276e3ae37840";
+        public static string API_KEY = "72f658b8bfmsh199cf55d6789f45p1006b5jsn5a4f417690cc";
         public static string CONTACT_US_EMAIL = "team5SCE@gmail.com";
         public static string CONTACT_US_PASSWORD = "OfirGilOmer123!";
     }
